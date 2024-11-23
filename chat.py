0bd59d650c329dc9208a51aa774a57070f6a34fc
Abhra Sarkar <abhrasarkar.official@gmail.com>
2024-11-23 15:31:31 +0530

    UI added

--- chat.py
+++ chat.py
@@ -3,6 +3,7 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
 from langchain_chroma import Chroma
 from models import Models
+import streamlit as st
 
 # Initialize the models
 models = Models()
@@ -19,7 +20,7 @@ vector_store = Chroma(
 # Define the chat prompt
 prompt = ChatPromptTemplate.from_messages(
     [
-        ("system", "You are a helpful assistant. Answer the question based only the data provided."),
+        ("system", "You are a helpful assistant (Co-Pilot). Answer the question based only the data provided."),
         ("human", "Use the user question {input} to answer the question. Use only the {context} to answer the question.")
     ]
 )
@@ -31,17 +32,32 @@ combine_docs_chain = create_stuff_documents_chain(
 )
 retrieval_chain = create_retrieval_chain(retriever, combine_docs_chain)
 
+
+######### Terminal Verison ###### [Run !python3.12 chat.py]
 # Main loop
-def main():
-    while True:
-        query = input("User (or type 'q', 'quit', or 'exit' to end): ")
-        if query.lower() in ['q', 'quit', 'exit']:
-            break
+# def main():
+#     while True:
+#         query = input("User (or type 'q', 'quit', or 'exit' to end): ")
+#         if query.lower() in ['q', 'quit', 'exit']:
+#             break
         
-        result = retrieval_chain.invoke({"input": query})
-        print("Assistant: ", result["answer"], "\n\n")
-        
-# Run the main loop
-if __name__ == "__main__":
-    main()
+#         result = retrieval_chain.invoke({"input": query})
+#         print("Assistant: ", result["answer"], "\n\n")
+
+# # Run the main loop
+# if __name__ == "__main__":
+#     main()
+
+import time
+######### UI Verison ###### [Run !python3.12 -m streamlit run chat.py]
+st.set_page_config(page_title="Document Co-Pilot")
+st.header("Document Co-Pilot")
+question =  st.text_input("Input", key="input")
+submit = st.button("Ask a question")
+if submit:
+    with st.spinner(":sunglasses: I'm on it, hold on a moment..."):
+        response = retrieval_chain.invoke({"input": question})
+        res=response['answer']
+    st.markdown(res)
+
 
